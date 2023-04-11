fb895e9da4e02cf1e03d1dffda6e2235f46003ea
Tobias Bleckert <hola@tobiasbleckert.se>
2023-04-11 07:48:50 +0200

    Refactored and added ID machines

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
-export { default } from './Machine.js';
-export { default as Store } from './Store.js';
+import Events from '@bleckert/events';
+
+export class Machine extends Events {
+    constructor(states, initial = 'IDLE', data = {}) {
+        super();
+
+        this.states = states;
+        this.initial = initial;
+        this.state = initial;
+        this._defaultData = { ...data };
+        this.set(this._defaultData);
+    }
+
+    can(action) {
+        return action in this.states[this.state].on;
+    }
+
+    isFinal() {
+        return this.states[this.state].final;
+    }
+
+    transition(prevState, input) {
+        if (prevState) {
+            this.emit(`${prevState}-OFF`);
+        }
+
+        this.emit(this.state, input);
+        this.emit('transition', this.state, prevState, input);
+    }
+
+    dispatch(actionName, input = null) {
+        if (!this.can(actionName)) {
+            return;
+        }
+
+        const currentState = this.state;
+        this.state = this.states[this.state].on[actionName];
+
+        this.transition(currentState, input);
+    }
+
+    start(input = null) {
+        this.transition(null, input);
+    }
+
+    reset(input = null) {
+        this.set({ ...this._defaultData });
+        this.state = this.initial;
+
+        this.start(input);
+    }
+
+    offState(state, cb) {
+        this.on(`${state}-OFF`, cb);
+    }
+
+    set(data) {
+        this.data = data;
+
+        Object.freeze(this.data);
+    }
+
+    setData(data) {
+        this.set({ ...this.data, ...data });
+        this.emit('data', data);
+    }
+}
+
+const store = {};
+
+export function createMachine(id, states, initial, data = {}) {
+    if (!store[id]) {
+        store[id] = new Machine(states, initial, data);
+    }
+
+    return store[id];
+}
---
+++ machine.test.js
@@ -0,0 +1,142 @@
+import { expect, test } from 'vitest';
+import { Machine, createMachine } from './index.js';
+
+const states = {
+    IDLE: {
+        on: {
+            send: 'SENDING',
+        },
+    },
+    SENDING: {
+        on: {
+            sent: 'SENT',
+            failed: 'ERROR',
+        },
+    },
+    SENT: {
+        final: true,
+    },
+    FAILED: {
+        on: {
+            retry: 'SENDING',
+        },
+    },
+};
+
+test('Can create a machine', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    expect(machine.state).toBe('IDLE');
+});
+
+test('Can transition to a new state', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.dispatch('send');
+
+    expect(machine.state).toBe('SENDING');
+});
+
+test('Can only transition to a valid state', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.dispatch('sent');
+
+    expect(machine.state).toBe('IDLE');
+});
+
+test('Can transition to a final state', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.dispatch('send');
+    machine.dispatch('sent');
+
+    expect(machine.state).toBe('SENT');
+    expect(machine.isFinal()).toBe(true);
+});
+
+test('Can listen to state changes', () => {
+    let called = false;
+    const machine = new Machine(states, 'IDLE');
+
+    machine.on('IDLE', () => {
+        called = true;
+    });
+
+    machine.start();
+
+    expect(called).toBe(true);
+});
+
+test('Can listen to leaving state', () => {
+    let called = false;
+    const machine = new Machine(states, 'IDLE');
+
+    machine.offState('IDLE', () => {
+        called = true;
+    });
+
+    machine.dispatch('send');
+    expect(called).toBe(true);
+});
+
+test('Can listen to state changes and unlisten', () => {
+    let called = false;
+    const machine = new Machine(states, 'IDLE');
+    const onSend = () => {
+        called = true;
+    };
+
+    machine.on('SENDING', onSend);
+    machine.off('SENDING', onSend);
+    machine.dispatch('send');
+
+    expect(called).toBe(false);
+});
+
+test('Can reset machine', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.dispatch('send');
+    expect(machine.state).toBe('SENDING');
+
+    machine.reset();
+    expect(machine.state).toBe('IDLE');
+});
+
+test('Can use machine storage', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.setData({
+        foo: 'bar',
+    });
+
+    expect(machine.data.foo).toBe('bar');
+});
+
+test('Can reset machine storage', () => {
+    const machine = new Machine(states, 'IDLE');
+
+    machine.setData({
+        foo: 'bar',
+    });
+
+    expect(machine.data.foo).toBe('bar');
+
+    machine.reset();
+
+    expect(machine.data.foo).toBe(undefined);
+});
+
+test('Can create multiple machines with same structure', () => {
+    const machine1 = createMachine('machine1', states, 'IDLE');
+    const machine2 = createMachine('machine2', states, 'IDLE');
+
+    expect(machine1.state).toBe('IDLE');
+    expect(machine2.state).toBe('IDLE');
+
+    machine1.dispatch('send');
+
+    expect(machine1.state).toBe('SENDING');
+    expect(machine2.state).toBe('IDLE');
+});
